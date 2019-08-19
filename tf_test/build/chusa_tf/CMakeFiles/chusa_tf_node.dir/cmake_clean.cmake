d8d14661562c28c58451187669491890ac4c4996
erichu92@naver.com <erichu92@naver.com>
2019-08-19 14:58:42 +0900

    odometry

---
+++ tf_test/build/chusa_tf/CMakeFiles/chusa_tf_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chusa_tf_node.dir/src/chusa_tf.cpp.o"
+  "/home/hmh/tf_test/devel/lib/chusa_tf/chusa_tf_node.pdb"
+  "/home/hmh/tf_test/devel/lib/chusa_tf/chusa_tf_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chusa_tf_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
