1450582b3e458fa434e2580a7cd296ddddc9f096
erichu92@naver.com <erichu92@naver.com>
2019-08-25 22:22:33 +0900

    fsdfs

--- tf_test/build/chusa_twist/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hmh/tf_test/src/chusa_twist
-# Build directory: /home/hmh/tf_test/build/chusa_twist
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
